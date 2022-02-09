c8518d1dbe3fa53a67b08894687422a6d17dac48
nguen1225 <nguen1225@gmail.com>
2022-02-09 09:16:32 +0900

    useStateの基本的な使い方

--- react_app/src/components/Article.jsx
+++ react_app/src/components/Article.jsx
@@ -1,10 +1,17 @@
-import {Content,Title} from "./entryPoint";
+import { useState } from "react";
+import {Content, Title, PublishButton} from "./entryPoint";
 
 const Article = (props) => {
+    const [isPublished, setPublished] = useState(false)
+    const publishArticle = () => {
+        setPublished(true)
+    }
+
     return (
         <div>
             <Title title = {props.title} />
             <Content content = {props.content} />
+            <PublishButton isPublished={isPublished} onClick={publishArticle} />
         </div>
     );
 };
