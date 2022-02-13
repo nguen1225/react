5f0152ccf2a8fe5c6fc38e1f800b737a12179c82
nguen1225 <nguen1225@gmail.com>
2022-02-13 21:14:04 +0900

    useEffectの使い方

--- react_app/src/components/Counter.jsx
+++ react_app/src/components/Counter.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Counter = () => {
     const [count,setCount] = useState(0)
+    const [count2,setCount2] = useState(0)
 
     const countUp = () => {
         setCount(prevState => prevState + 1)
@@ -10,6 +11,34 @@ const Counter = () => {
     const countDown = () => {
         setCount(prevState => prevState -1)
     }
+
+    const count2Up = () => {
+        setCount2(prevState => prevState + 2)
+    }
+
+    const count2Down = () => {
+        setCount2(prevState => prevState -2)
+    }
+
+    // 毎回レンダリングされるごとに実行される
+    useEffect(() => {
+        console.log("毎回実行される", count)
+    })
+
+    // 初回レンダリング後(それ以降は実行されない)のみ実行される
+    useEffect(() => {
+        console.log("レンダリング後のみ実行される", count)
+    }, [])
+
+    // countが変更される度に実行される
+    useEffect(() => {
+        console.log("現在のカウント数は", count)
+    }, [count])
+
+    // count1かcount2が変更される度に実行される
+    // useEffect(() => {
+    //     console.log("現在のカウント数は", count)
+    // }, [count1, count2])
 
     return (
         <div>
@@ -21,6 +50,14 @@ const Counter = () => {
             </p>
             <button onClick={countUp}>up</button>
             <button onClick={countDown}>down</button>
+            <h2>
+                カウンター2
+            </h2>
+            <p>
+                現在のカウント：{count2}
+            </p>
+            <button onClick={count2Up}>up</button>
+            <button onClick={count2Down}>down</button>
         </div>
     )
 }
